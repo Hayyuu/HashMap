70e8f1d75ba104bfbfd55c6a102a7d75b381a97a
Hayyuu <hayatahimed518@gmail.com>
2024-10-05 13:30:33 +0300

    Update implementation of functions incorporating hash key

--- hashMap.mjs
+++ hashMap.mjs
@@ -1,5 +1,5 @@
 class HashMap{
-    constructor(bucketArr=[[]],length=0){
+    constructor(bucketArr=[],length=0){
         this.bucketArr=bucketArr;
         this.length=length;
         this.maxLength=16;
@@ -14,60 +14,83 @@ class HashMap{
         for (let i = 0; i < key.length; i++) {
           hashCode = primeNumber * hashCode + key.charCodeAt(i);
         }
-     
         return hashCode;
     }
     set(key,value){
-       
-        if(this.length==0){
-            this.bucketArr=[[key,value]];
-            
+        let hashKey=this.hash(key);
+        hashKey=hashKey%16;
+        if(this.has(hashKey)){
+            this.bucketArr[hashKey][1]=value;
+            this.length--;
         }
-        else if(this.has(key)){
-            for(let i=0;i<this.size()-1;i++){
-                if(this.bucketArr[i][0]==key){
-                    this.bucketArr[i][1]=value;
-                }        
-            }
-        }
-        else{
-            this.bucketArr.push([key,value]);
+            this.bucketArr[hashKey]=[key,value];
             this.length++;
-        }
-        this.length++;
+            
         
         return this.bucketArr;
     }
     get(key){
-        for(let i=0;i<this.size()-1;i++){
-            if(this.bucketArr[i][0]==key){
-                return this.bucketArr[i][1];
-            }
-            
-        }
+        key=key%16;
+        return this.bucketArr[key][1];   
     }
     has(key){
         let exists=false;
-        for(let i=0;i<this.size()-1;i++){
-            if(this.bucketArr[i][0]==key){
-                exists=true;
-                return exists;
+        key=key%16;
+        if(this.bucketArr[key]){
+            exists=true;
+            return exists;
             }
-        }
+    
         return exists;
+    }
+    values(){
+        let valuesArr=[];
+        for(let i=0;i<this.maxLength;i++){
+            if(this.bucketArr[i]){
+                valuesArr.push(this.bucketArr[i][1]);
+            }
+            
+        }
+        return valuesArr;
+    }
+    keys(){
+        let keysArr=[];
+        for(let i=0;i<this.maxLength;i++){
+            if(this.bucketArr[i]){
+                keysArr.push(this.bucketArr[i][0]);
+            }
+            
+        }
+        return keysArr;
+    }
+    clear(){
+        this.bucketArr=[];
+        this.length=0;
+        return this;
+    }
+    entries(){
+        return this.bucketArr;
     }
     
 }
 
 let hashMap=new HashMap();
 
-console.log(hashMap.hash('banana'));
 
 console.log(hashMap.set('banana','yellow'));
 console.log(hashMap.set('apple','red'));
 console.log(hashMap.set('apple','pink'));
 
-console.log(hashMap.has('apple'));
-console.log(hashMap.get('banana'))
+console.log("The Map contains 'apple' ?",hashMap.has(hashMap.hash('apple')));
+console.log("The value for the key 'banana' is ",hashMap.get(hashMap.hash('banana')));
+
+console.log("The Hash Map",hashMap);
+console.log("Entries" ,hashMap.entries());
+
+console.log("Values" ,hashMap.values());
+console.log("Keys" ,hashMap.keys());
+
+console.log(hashMap.clear());
+
 
 
